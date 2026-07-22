e4a15c112b3328e3b91d46ec2c2b979c0fcb6596
shirko21 <125873528+shirko21@users.noreply.github.com>
2026-07-22 18:47:29 +0330

    Delete src directory

--- src/routes/admin.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const auth = require("../middleware/auth");
-const adminController = require("../controllers/admin.controller");
-
-router.get("/dashboard", auth, adminController.dashboard);
-
-module.exports = router;
